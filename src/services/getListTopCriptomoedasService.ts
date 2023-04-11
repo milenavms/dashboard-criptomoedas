576f73b72b64baff385ebf2bf9a26010699b4752
milena <milenasantos@bemol.com.br>
2023-04-11 09:37:53 -0400

    Add type interface
    Add type interface

--- src/services/getListTopCriptomoedasService.ts
+++ src/services/getListTopCriptomoedasService.ts
@@ -17,7 +17,7 @@ interface CriptoListInterface{
     data: CriptoTopListInterface[]
 }
 
-export async function getListTopCriptomoedasService(): Promise<CriptoListInterface>{
+export async function getListTopCriptomoedasService(): Promise<any>{
     const endpoint = "https://api.coingecko.com/api/v3/coins/markets/?vs_currency=usd";
 
     const response = await axios.get<CriptoListInterface>(endpoint);
